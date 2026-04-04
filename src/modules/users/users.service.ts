2c5f604d7e0100b974c87303cbeb2e9026202e2b
Lithira-Jayanaka <kariyawasamlithira2@gmail.com>
2026-04-04 16:25:57 +0530

    feat: integrate Prisma module and implement user creation and retrieval methods in UsersService

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../../prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
+import { RegisterRole } from '../auth/dto/register.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  async createUser(name: string, email: string, password: string, role: RegisterRole) {
+  const hashed = await bcrypt.hash(password, 10);
+
+  return this.prisma.user.create({
+    data: { name, email, password: hashed, role },
+  });
+}
+
+  async findByEmail(email: string) {
+    return this.prisma.user.findUnique({ where: { email } });
+  }
+}
