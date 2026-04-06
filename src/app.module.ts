4b9f9a4d06fb06949e4eedfdf44a3933a6948e58
Lithira-Jayanaka <kariyawasamlithira2@gmail.com>
2026-04-06 23:01:03 +0530

    feat: add Assignments module with controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
+import { AssignmentsModule } from './modules/assignments/assignments.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, UsersModule],
+  imports: [PrismaModule, AuthModule, UsersModule, AssignmentsModule],
 })
 export class AppModule {}
